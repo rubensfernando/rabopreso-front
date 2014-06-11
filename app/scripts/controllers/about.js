2a634309324b6b04034ed34222dd4dc80482fae3
Rubens Fernando <rubensfernandom@gmail.com>
2014-06-10 23:42:38 -0300

    Second commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name raboPresoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the raboPresoApp
+ */
+angular.module('rabopresoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name raboPresoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the raboPresoApp
+ */
+angular.module('rabopresoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
